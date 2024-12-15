f60fff85f42296a98d263f9fe85bdc3df6b9699d
rogeriop <pinheiroxrogerio@gmail.com>
2024-12-15 17:13:44 -0300

    02. Assincronicidade

--- scripts.js
+++ scripts.js
@@ -4,3 +4,35 @@ const inputUpload = document.getElementById("image-upload")
 uploadBtn.addEventListener("click", () => {
     inputUpload.click();
 })
+
+function lerConteudoDoArquivo(arquivo) {
+    return new Promise((resolve, reject) => {
+        const leitor = new FileReader();
+        leitor.onload = () => {
+            resolve({ url: leitor.result, name: arquivo.name})
+        }
+
+        leitor.onerr = () => {
+            reject(`Erro na leitura do arquivo ${arquivo.name}`)
+        }
+        leitor.readAsDataURL(arquivo);
+    })
+
+}
+
+const imagemPrincipal = document.querySelector(".main-imagem");
+const nomeDaImagem = document.querySelector(".container-imagem-nome p");
+
+
+inputUpload.addEventListener("change", async (evento) => {
+    const arquivo = evento.target.files[0];
+    if (arquivo) {
+        try {
+            const conteudoDoArquivo = await lerConteudoDoArquivo(arquivo);
+            imagemPrincipal.src = conteudoDoArquivo.url;
+            nomeDaImagem.textContent = conteudoDoArquivo.name;
+        } catch {
+            console.error("Erro na leitura do arquivo")
+        }
+    }    
+})
